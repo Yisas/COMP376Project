8e387244e2fe32ceef52acf3191884c6b1726d54
francisct <franciscotetremblay@gmail.com>
2016-10-15 19:52:50 -0400

    added tile prefab added facedirection added colliders to players and tiles fixed player not moving

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
-
+    Vector3 direction;
     // Private references
     private Rigidbody2D rb;
+	private Transform transform;
+    Transform sprites;
     private Animator anim;
 
     private float moveInput = 0.0f;
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour {
         // Setup references
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+        transform = GetComponent<Transform>();
+        sprites = transform.FindChild("Sprites");
 	}
 	
 	// Update is called once per frame
@@ -37,9 +41,22 @@ public class PlayerController : MonoBehaviour {
 
     private void Move()
     {
+        direction = new Vector2(moveInput, 0.0f);
         // Horizontal movement
-        rb.AddForce(new Vector2(moveInput * moveSpeed, 0));
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
+        FaceDirection (direction);
+        //rb.AddForce(new Vector2(moveInput * moveSpeed, 0));
         // Pass movement speed to animator
         anim.SetFloat("speed",Mathf.Abs(moveInput));
     }
+
+	private void FaceDirection(Vector2 direction)
+	{
+        if (direction.x != 0.0f)
+        {
+            Quaternion rotation3D = direction.x > 0 ? Quaternion.LookRotation(Vector3.forward) : Quaternion.LookRotation(Vector3.back);
+            sprites.rotation = rotation3D;
+        }
+    }
+    
 }
