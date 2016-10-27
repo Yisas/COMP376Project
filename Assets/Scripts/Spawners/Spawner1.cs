c9ac0626bb4fb6e5b836c655f8c584247792130b
Brandon Mendez <b.e.mendez@outlook.com>
2016-10-26 21:06:15 -0400

    #2 Fixed the transform of the players in the hierarchy, added two spawner objects and started working on spawning players

---
+++ Assets/Scripts/Spawners/Spawner1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner1 : MonoBehaviour {
+
+    public GameObject player1;
+
+	// Use this for initialization
+	void Start () {
+        SpawnPlayer();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckIfDead();
+	}
+
+    void CheckIfDead()
+    {
+
+    }
+
+    void SpawnPlayer()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(180, 158, 0));
+        GameObject newPlayer1 = Instantiate(player1, ray.GetPoint(5), Quaternion.identity) as GameObject;
+        newPlayer1.transform.parent = GameObject.Find("Players").transform;
+        Debug.Log("Player spawned");
+    }
+}
---
+++ Assets/Scripts/Spawners/Spawner2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner2 : MonoBehaviour {
+
+    public GameObject player2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
