cb4209e2f29d7e730fb97099caf1476b963c3249
francisct <franciscotetremblay@gmail.com>
2016-10-27 12:20:12 -0400

    added follow cam

---
+++ Assets/Scripts/Camera/FollowCam.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+    
+    Transform follow;
+    Transform other;
+    public float smoothness;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        
+        if (follow != null && other != null)
+        {
+            Vector3 between = new Vector3(follow.position.x + (other.position.x - follow.position.x) / 2, follow.position.y + (other.position.y - follow.position.y) / 2, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, between, smoothness * Time.deltaTime);
+        }
+       
+    }
+
+    public void Follow(Transform follow, Transform other)
+    {
+        this.follow = follow;
+        this.other = other;
+    }
+
+    public void StopFollowing()
+    {
+        this.follow = null;
+        this.other = null;
+    }
+
+
+}
--- Assets/Scripts/Spawners/Spawner1.cs
+++ Assets/Scripts/Spawners/Spawner1.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class Spawner1 : MonoBehaviour {
 
-    public GameObject player1;
+    public GameObject[] players;
+    public Transform[] spawnPoints;
+    public FollowCam cam;
 
 	// Use this for initialization
 	void Start () {
-        SpawnPlayer();
+        SpawnPlayers();
     }
 	
 	// Update is called once per frame
@@ -20,11 +22,27 @@ public class Spawner1 : MonoBehaviour {
 
     }
 
-    void SpawnPlayer()
+    void SpawnPlayers()
     {
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(180, 158, 0));
-        GameObject newPlayer1 = Instantiate(player1, ray.GetPoint(5), Quaternion.identity) as GameObject;
+
+        /*for (int i = 0; i < players.Length && i < spawnPoints.Length; i++)
+        {
+            GameObject newPlayer = Instantiate(players[i], spawnPoints[i].position, Quaternion.identity) as GameObject;
+            newPlayer.transform.parent = GameObject.Find("Players").transform;
+            Debug.Log("Player spawned");
+            
+        }*/
+        
+        //When combat system is functionnal, uncomment use upper part. follow player should be handled by the combat sytem manager.
+        GameObject newPlayer1 = Instantiate(players[0], spawnPoints[0].position, Quaternion.identity) as GameObject;
         newPlayer1.transform.parent = GameObject.Find("Players").transform;
         Debug.Log("Player spawned");
+
+        GameObject newPlayer2 = Instantiate(players[1], spawnPoints[1].position, Quaternion.identity) as GameObject;
+        newPlayer2.transform.parent = GameObject.Find("Players").transform;
+        Debug.Log("Player spawned");
+
+        cam.Follow(newPlayer1.transform, newPlayer2.transform);
+
     }
 }
