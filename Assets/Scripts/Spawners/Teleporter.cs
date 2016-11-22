4f3df2cd5da43c8b2d7eacb7b7a52ba36de8dcbe
Brandon Mendez <b.e.mendez@outlook.com>
2016-11-22 13:28:45 -0500

    #2 Fixed teleporter script to properly detect which player is dead in order to properly teleport players to the next stage

--- Assets/Scripts/Spawners/Teleporter.cs
+++ Assets/Scripts/Spawners/Teleporter.cs
@@ -34,7 +34,7 @@ public class Teleporter : MonoBehaviour {
             {
                 players = FindObjectsOfType<PlayerController>();
                 Debug.Log("Player[0]: " + players[0].playerNumber + " and Player[1]: " + players[1].playerNumber);
-                playerTimer = false;
+                playerTimer = false; //only want it to run this once
             }
         }
     }
@@ -59,15 +59,32 @@ public class Teleporter : MonoBehaviour {
 
     private void ProgressToNextStage()
     {
-        if (players[0] == null && teleporterNumber == 2) //player 2 dead
+        if (players[0] == null && players[1].playerNumber == 1 && teleporterNumber == 2) //player 2 dead
         {
             Debug.Log("Player 1 is trying to progress");
+         // LoadNextStage("right1");
+        }
+        else if(players[1] == null && players[0].playerNumber == 1 && teleporterNumber == 2)
+        {
+            Debug.Log("Player 1 is trying to progress");
+            // LoadNextStage("right1");
         }
 
-        if(players[1] == null && teleporterNumber == 1) //player 1 dead
+        if (players[1] == null && players[0].playerNumber == 2 && teleporterNumber == 1) //player 1 dead
         {
             Debug.Log("Player 2 is trying to progress");
+            //LoadNextStage("left1");
         }
+        else if(players[0] == null && players[1].playerNumber == 2 && teleporterNumber == 1)
+        {
+            Debug.Log("Player 2 is trying to progress");
+            //LoadNextStage("left1");
+        }
+    }
+
+    private void LoadNextStage(string stageName)
+    {
+        Application.LoadLevel(stageName);
     }
 
     void OnTriggerEnter2D (Collider2D collider)
