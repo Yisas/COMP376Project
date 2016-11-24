5429e5acab6a478ace579d0b7598371c0265473c
francisct <franciscotetremblay@gmail.com>
2016-11-24 15:22:15 -0500

    #2 fixed character spwaning. Teleportaion now supports persistance

--- Assets/Scripts/Spawners/Spawner1.cs
+++ Assets/Scripts/Spawners/Spawner1.cs
@@ -3,24 +3,20 @@ using System.Collections;
 
 public class Spawner1 : MonoBehaviour {
 
-    public GameObject[] players;
+    public GameObject[] playerTemplates;
     public Transform[] spawnPoints;
     public FollowCam cam;
 
     GameObject newPlayer1;
     GameObject newPlayer2;
-    
-    public LayerMask player1Layer;
-    public LayerMask player2Layer;
 
-    PlayerController[] player;
-    int playerNumber;
+    PlayerController[] players;
     bool deadPlayer = false;
 
     // Use this for initialization
     void Start () {
         SpawnPlayers();
-        player = FindObjectsOfType<PlayerController>();
+        players = FindObjectsOfType<PlayerController>();
     }
 	
 	// Update is called once per frame
@@ -32,15 +28,15 @@ public class Spawner1 : MonoBehaviour {
     {
         if (deadPlayer == false)
         {
-            if (player[0] == null) //if player 2 is dead
+            if (players[0] == null) //if player 2 is dead
             {
-                int playerNum = player[0].playerNumber;
+                int playerNum = players[0].playerNumber;
                 StartCoroutine(SpawnPlayer(playerNum));
                 deadPlayer = true;
             }
-            if (player[1] == null) //if player 1 is dead
+            if (players[1] == null) //if player 1 is dead
             {
-                int playerNum = player[1].playerNumber;
+                int playerNum = players[1].playerNumber;
                 StartCoroutine(SpawnPlayer(playerNum));
                 deadPlayer = true;
             }
@@ -51,30 +47,30 @@ public class Spawner1 : MonoBehaviour {
     {
         bool dontSpawnP1 = false;
         bool dontSpawnP2 = false;
-        PlayerController[] players = FindObjectsOfType<PlayerController>();
-        if (players.Length == 1)
+        PlayerController[] persistingPlayer = FindObjectsOfType<PlayerController>();
+        if (persistingPlayer.Length == 1)
         {
-            if (players[0].playerNumber == 1)
+            if (persistingPlayer[0].playerNumber == 1)
             {
-                newPlayer1 = players[0].gameObject;
+                newPlayer1 = persistingPlayer[0].gameObject;
                 newPlayer1.transform.position = spawnPoints[0].position;
                 dontSpawnP1 = true;
             }
-            else if (players[0].playerNumber == 2)
+            else if (persistingPlayer[0].playerNumber == 2)
             {
-                newPlayer2 = players[0].gameObject;
+                newPlayer2 = persistingPlayer[0].gameObject;
                 newPlayer2.transform.position = spawnPoints[1].position;
                 dontSpawnP2 = true;
             }
         }
 
         if (!dontSpawnP1) { 
-        newPlayer1 = Instantiate(players[0], spawnPoints[0].position, Quaternion.identity) as GameObject;
+        newPlayer1 = Instantiate(playerTemplates[0], spawnPoints[0].position, Quaternion.identity) as GameObject;
         Debug.Log("Player spawned");
         }
         if (!dontSpawnP2)
         {
-            newPlayer2 = Instantiate(players[1], spawnPoints[1].position, Quaternion.identity) as GameObject;
+            newPlayer2 = Instantiate(playerTemplates[1], spawnPoints[1].position, Quaternion.identity) as GameObject;
             Debug.Log("Player spawned");
         }
         spawnPoints[0].transform.position += new Vector3(-39, 0, 0);
@@ -85,20 +81,19 @@ public class Spawner1 : MonoBehaviour {
 
     IEnumerator SpawnPlayer(int playerNum)
     {
-        playerNumber = playerNum;
         Debug.Log("Waiting to respawn player");
 
         yield return new WaitForSeconds(3f);
-        if (playerNumber == 1) //if player 1 is dead spawn him
+        if (playerNum == 1) //if player 1 is dead spawn him
         {
-            newPlayer1 = Instantiate(players[0], spawnPoints[0].position, Quaternion.identity) as GameObject;
+            newPlayer1 = Instantiate(playerTemplates[0], spawnPoints[0].position, Quaternion.identity) as GameObject;
         }
         else //if player 2 is dead spawn him
         {
-            newPlayer2 = Instantiate(players[1], spawnPoints[1].position, Quaternion.identity) as GameObject;
+            newPlayer2 = Instantiate(playerTemplates[1], spawnPoints[1].position, Quaternion.identity) as GameObject;
         }
 
-        player = FindObjectsOfType<PlayerController>(); //reasign either player[0] or player[1] to not null
+        players = FindObjectsOfType<PlayerController>(); //reasign either player[0] or player[1] to not null
         cam.Follow(newPlayer1.transform, newPlayer2.transform);
         deadPlayer = false;
     }
