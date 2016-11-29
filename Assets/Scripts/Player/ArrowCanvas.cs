5bb68d4bd00af2a51c45509b3ef5aeb13dfe85ad
Jesus Imery <yisas_0929@hotmail.com>
2016-11-29 16:25:27 -0500

    #2 Added arrow canvas

---
+++ Assets/Scripts/Player/ArrowCanvas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowCanvas : MonoBehaviour {
+
+	public GameObject arrowLeft;
+	public GameObject arrowRight;
+
+	// Use this for initialization
+	void Start () {
+		arrowLeft = transform.FindChild ("Arrow Left").gameObject;
+		arrowRight = transform.FindChild ("Arrow Right").gameObject;
+	}
+	
+	public void SetRightArrow(bool active)
+	{
+		arrowRight.SetActive (active);
+	}
+
+	public void SetLeftArrow(bool active)
+	{
+		arrowLeft.SetActive (active);
+	}
+}
